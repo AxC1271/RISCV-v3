f0bcccaf0c11848311b77e44527818a55c57f518
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2026-08-20 16:45:45 -0400

    finished ifid stage

--- 0-rtl/ifid_stage.sv
+++ 0-rtl/ifid_stage.sv
@@ -1,6 +1,20 @@
 module ifid_stage (
     input  logic clk,
     input  logic rst_n,
+    input  logic stall,
+    input  logic flush,
+    input  logic [31:0] if1_pc,
+    input  logic [31:0] if1_instr,
+    input  logic        if1_valid,
+    input  logic [31:0] if2_pc,
+    input  logic [31:0] if2_instr,
+    input  logic        if2_valid,
+    output logic [31:0] id1_pc,
+    output logic [31:0] id1_instr,
+    output logic        id1_valid,
+    output logic [31:0] id2_pc,
+    output logic [31:0] id2_instr,
+    output logic        id2_valid
 );
 
     /*
@@ -8,6 +22,26 @@ module ifid_stage (
     2. All combinational/registered logic are defined here
     3. Use these during testbenches/simulations 
     */
+
+    always_ff @(posedge clk) begin
+        if (!rst_n || flush) begin
+            id1_pc    <= 32'h0;
+            id1_instr <= 32'h00000013;
+            id1_valid <= 1'b0;  
+            id2_pc    <= 32'h0;
+            id2_instr <= 32'h00000013;
+            id2_valid <= 1'b0;           
+        end else if (stall) begin
+            // hold
+        end else begin
+            id1_pc    <= if1_pc;
+            id1_instr <= if1_instr;
+            id1_valid <= if1_valid;
+            id2_pc    <= if2_pc;
+            id2_instr <= if2_instr;
+            id2_valid <= if2_valid;
+        end
+    end
 
     /*
     1. My formal properties live here
--- 0-rtl/register_file.sv
+++ 0-rtl/register_file.sv
@@ -13,14 +13,23 @@ module register_file (
     output logic[31:0] rd2_data2,
 
     // write ports, also dual
-    
+    input  logic[4:0]  wr1_addr,
+    input  logic[4:0]  wr2_addr,
+    input  logic[31:0] wr1_data,
+    input  logic[31:0] wr2_data,
+    input  logic       reg1_write,
+    input  logic       reg2_write
 );
-
     /*
     1. All of my RTL logic lives here
     2. All combinational/registered logic are defined here
     3. Use these during testbenches/simulations 
     */
+
+    // define the same 32 architectural registers
+    logic[31:0] registers [0:31];
+
+    
 
     /*
     1. My formal properties live here
